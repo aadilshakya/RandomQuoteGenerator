e4aea2913599103f349942769ac2acef11a71236
smilerac <smilerac15@gmail.com>
2021-02-15 10:02:26 +0545

    created a react app

--- randgen-react/src/Quotes/Quotes.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const quotes= (props) => {
-    return (<div>
-        <p className="quotations">{props.quotation}</p>
-        <p className="cite">{props.author}</p>
-    </div>);
-}
-export default quotes;
